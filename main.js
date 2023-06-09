1fd4ed8d1356b014dc5110ada1b0298c86601074
mariascherrer <vitorianuvemmatos2@gmail.com>
2023-06-09 13:58:52 -0300

    inital commit

---
+++ main.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+
+    function showMessage(id){
+
+        if(id == 1){
+            alert("mensagem 1");
+        }else if(id == 2){
+            alert("mensagem 2");
+        }if(id == 3){
+            alert("mensagem 3");
+        }if(id == 4){
+            alert("mensagem 4");
+        }
+
+    }
+
+}
